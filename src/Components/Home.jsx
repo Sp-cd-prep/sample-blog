508f1838dd89a3f6890a67d456878892306071a7
SP Acharya <sthitaprangya.acharya@collegedekho.com>
2024-10-21 20:16:26 +0530

    hamburger

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
 import './component.css';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Home = () => {
   return (
@@ -26,6 +32,41 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+    <CarouselProvider
+     naturalSlideWidth={100}
+     naturalSlideHeight={10}
+     totalSlides={3}
+    >
+
+    <Slider>
+      <Slide index={0}><img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/autumn-background-with-space-for-text-and-leaves-around-free-image.jpeg?w=600&quality=80"/></Slide>
+      <Slide index={1}><img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/autumn-background-with-space-for-text-and-leaves-around-free-image.jpeg?w=600&quality=80"/></Slide>
+      <Slide index={2}><img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/autumn-background-with-space-for-text-and-leaves-around-free-image.jpeg?w=600&quality=80"/></Slide>
+    </Slider>
+
+    <ButtonBack>Back</ButtonBack>
+    <ButtonNext>Next</ButtonNext>
+   </CarouselProvider>
+
+
+
+
+   <Carousel>
+      <div>
+         <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/autumn-background-with-space-for-text-and-leaves-around-free-image.jpeg?w=600&quality=80" />
+         <p className="legend">Legend 1</p>
+       </div>
+       <div>
+         <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/autumn-background-with-space-for-text-and-leaves-around-free-image.jpeg?w=600&quality=80" />
+         <p className="legend">Legend 2</p>
+       </div>
+      <div>
+      <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/autumn-background-with-space-for-text-and-leaves-around-free-image.jpeg?w=600&quality=80" />
+      <p className="legend">Legend 3</p>
+     </div>
+   </Carousel>
+
     </>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,68 +1,76 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import './component.css';
+import "./component.css";
+import { useEffect } from "react";
 
 const Navbar = () => {
+
+
+
+  useEffect(()=>{
+      const hamburger = document.querySelector('.hamburger');
+      const navMenu = document.querySelector('.nav-menu');
+
+
+      const toggleMenu  =()=>{
+        hamburger.classList.toggle('hamActive');
+        navMenu.classList.toggle('hamActive')
+      }
+
+      const closeMenu=()=>{
+        hamburger.classList.remove('hamActive');
+        navMenu.classList.remove('hamActive')
+      }
+
+      if(hamburger){
+        hamburger.addEventListener('click',toggleMenu)
+      }
+
+      document.querySelectorAll('.nav-item').forEach((close)=>{
+        close.addEventListener('click',closeMenu)
+      })
+
+
+      return ()=>{
+        if(hamburger){
+          hamburger.removeEventListener('click',toggleMenu)
+        }
+
+        document.querySelectorAll('.nav-item').forEach((close)=>{
+          close.removeEventListener('click',closeMenu)
+        })
+      }
+  },[])
+
+
+
+
+
   return (
     <>
-     <div className="header">
-            <div className="logo">
-                <span className='verticalText'>The</span>
-                <h1>Siren</h1>
-            </div>
-    <nav>
-      <ul className="nav-menu">
-        <li>
-          <NavLink
-            to="/"
-            style={({ isActive }) => ({ color: isActive ? "black" : "brown" })}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/bollywood"
-            style={({ isActive }) => ({ color: isActive ? "black" : "brown" })}
-          >
-            Bollywood
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/hollywood"
-            style={({ isActive }) => ({ color: isActive ? "black" : "brown" })}
-          >
-            Hollywood
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/tech"
-            style={({ isActive }) => ({ color: isActive ? "black" : "brown" })}
-          >
-            Technology
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/fitness"
-            style={({ isActive }) => ({ color: isActive ? "black" : "brown" })}
-            >
-            Fitness
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/food"
-            style={({ isActive }) => ({ color: isActive ? "black" : "brown" })}
-          >
-            Food
-          </NavLink>
-        </li>
-      </ul>
-      </nav>
-    </div>
+      <div className="header">
+        <div className="logo">
+          <span className="verticalText">The</span>
+          <h1>Siren</h1>
+        </div>
+        <nav>
+          <ul className="nav-menu">
+            <li className="nav-item"><NavLink to="/" >Home</NavLink></li>
+            <li className="nav-item"><NavLink to="/bollywood" >Bollywood</NavLink></li>
+            <li className="nav-item"><NavLink to="/hollywood" >Hollywood</NavLink></li>
+            <li className="nav-item"><NavLink to="/tech" >Technology</NavLink></li>
+            <li className="nav-item"><NavLink to="/fitness" > Fitness</NavLink></li>
+            <li className="nav-item"><NavLink to="/food" >Food</NavLink></li>
+          </ul>
+
+          <div className="hamburger">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+          </div>
+
+        </nav>
+      </div>
     </>
   );
 };
